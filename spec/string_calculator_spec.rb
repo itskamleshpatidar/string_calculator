953565a925f7200c4603aa5561ceb13545f40203
kamlesh <itskamleshpatidar@gmail.com>
2024-12-19 17:50:00 +0530

    handle negative numbers

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -24,5 +24,13 @@ RSpec.describe StringCalculator do
     it 'supports different delimiters' do
       expect(StringCalculator.add("//;\n1;2")).to eq(3)
     end
+
+    it 'raises an exception for negative numbers' do
+      expect { StringCalculator.add("1,-2,3") }.to raise_error("negative numbers not allowed: -2")
+    end
+
+    it 'raises an exception with all negative numbers in the message' do
+      expect { StringCalculator.add("1,-2,-3") }.to raise_error("negative numbers not allowed: -2,-3")
+    end
   end
 end
