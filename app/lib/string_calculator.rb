ceaefa4884c4ddec87d528217fdd46e04e17b487
kamlesh <itskamleshpatidar@gmail.com>
2024-12-19 16:39:00 +0530

    Implementation of Handle multiple numbers

--- app/lib/string_calculator.rb
+++ app/lib/string_calculator.rb
@@ -3,5 +3,12 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
     return numbers.to_i if numbers.length == 1
+    split_numbers(numbers).map(&:to_i).reduce(:+)
+  end
+
+  private
+
+  def self.split_numbers(numbers)
+    numbers.split(',')
   end
 end
