b0eda039a4cf433d80cc2d8accc493fee9e6796f
kamlesh <itskamleshpatidar@gmail.com>
2024-12-19 17:31:00 +0530

    update implementation of Support different delimiters

--- app/lib/string_calculator.rb
+++ app/lib/string_calculator.rb
@@ -3,12 +3,24 @@ class StringCalculator
   def self.add(numbers)
     return 0 if numbers.empty?
     return numbers.to_i if numbers.length == 1
-    split_numbers(numbers).map(&:to_i).reduce(:+)
+
+    delimiter, numbers = extract_delimiter(numbers)
+    split_numbers(numbers, delimiter).map(&:to_i).reduce(:+)
   end
 
   private
 
-  def self.split_numbers(numbers)
-    numbers.split(/,|\n/)
+  def self.split_numbers(numbers, delimiter)
+    numbers.split(delimiter)
+  end
+
+  def self.extract_delimiter(numbers)
+    if numbers.start_with?("//")
+      delimiter = numbers[2]
+      numbers = numbers.split("\n", 2).last
+    else
+      delimiter = /,|\n/
+    end
+    [delimiter, numbers]
   end
 end
